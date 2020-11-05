af1c9ff5c412c85e67fc633ee038c51a89ab80ec
Benjamin Bulis <beni1999@gmx.at>
2020-11-05 21:50:15 +0100

    Added Comments to functions

--- src/Model.py
+++ src/Model.py
@@ -3,24 +3,38 @@ from POI import POI
 from PyQt5.QtWidgets import QTableWidgetItem
 
 class Model:
-    pois_objects = []
+    """
+    Model welches alle Daten und alle wichtigen Funktionen beinhaltet
+    """
+    pois_objects = []   # Liste welche alle Daten beim Einlesen der File beinhaltet
 
     def __init__(self):
         pass
 
     def read_file_content(self, fileName):
+        """
+        Funktion welche das File einliest und die Daten weiter gibt
+        :param fileName: der Name der Datei welche die Daten beinhaltet
+        :return:
+        """
         with open(fileName, "r", encoding="utf-8") as file:
-            content = csv.reader(file, delimiter='|')
-            self.pois_objects.clear()
+            content = csv.reader(file, delimiter='|')   # Konfiguration des csv-Readers um das Dateiformat lesen zu können
+            self.pois_objects.clear() # Leeren der Liste um diese für die nächsten Daten bereit zu halten
             for row in content:
                 self.transform_file_content_to_poi_object(row)
 
     def save_table_content_to_file(self, filename, table):
+        """
+        Funktion um alle Daten der Tabelle in ein File zu speichern
+        :param filename: Der Name der Datei welche alle Daten später beinhaltetn soll
+        :param table: Die Tabelle von welcher alle Daten kommen
+        :return:
+        """
         rowCount = table.rowCount()
         rowCounter = 0
 
         with open(filename, "w", encoding="utf-8") as file:
-            writer = csv.writer(file, delimiter='|', quoting=csv.QUOTE_NONE)
+            writer = csv.writer(file, delimiter='|', quoting=csv.QUOTE_NONE)    # Konfiguration des csv-Writers
             while rowCounter < rowCount:
                 category = table.item(rowCounter, 1).text()
                 name = table.item(rowCounter, 0).text()
@@ -49,17 +63,23 @@ class Model:
                                  street,
                                  housenumber,
                                  info,
-                                 phonenumber])
+                                 phonenumber,
+                                 '']) # Schreiben der eigentlichen Zeile Zeile im File
                 rowCounter = rowCounter + 1
 
     def print_content_from_list_to_table(self, table):
+        """
+        Funktion um alle Daten des File in die Tabelle zu schreiben
+        :param table: Die Tabelle welche alle Daten des Files bekommt
+        :return:
+        """
         rowCount = table.rowCount()
         if rowCount > 0:
             for i in range(0, rowCount-1):
                 table.removeRow(0)
                 table.setRowCount(0)
 
-        for idx, val in enumerate(self.pois_objects):
+        for idx, val in enumerate(self.pois_objects):   # Iteration der Liste welche alle Daten beinhaltet um diese in die Tabelle eintragen zu können
             table.insertRow(idx)
             table.setItem(idx, 0,QTableWidgetItem(val.get_name()))
             table.setItem(idx, 1, QTableWidgetItem(val.get_category()))
@@ -75,14 +95,23 @@ class Model:
             table.setItem(idx, 11, QTableWidgetItem(val.get_icon()))
 
     def delete_every_poi_from_table(self, table):
+        """
+        Funktion um die ganze Tabelle und die Liste zu leeren
+        :param table: Tabelle welche geleert werden soll
+        :return:
+        """
         rowCount = table.rowCount()
-        print(rowCount)
         if rowCount > 0:
             for i in range(0, rowCount):
                 table.removeRow(0)
                 table.setRowCount(0)
 
     def transform_file_content_to_poi_object(self, item):
+        """
+        Funktion welche die Daten der Datei in ein POI Objekt umwandelt
+        :param item: Das Item welches in ein POI Objekt umgewandelt werden soll
+        :return:
+        """
         id = item[0]
         category = item[1]
         name = item[2]
@@ -100,8 +129,16 @@ class Model:
         self.pois_objects.append(poi)
 
     def get_pois(self):
+        """
+        Funktion welche die Liste zurück gibt
+        :return: Die Liste der POI Objekte
+        """
         return self.pois_objects
 
     def print_pois(self):
+        """
+        Funktion welche die POI Objekte auf der Konsole ausgibt
+        :return:
+        """
         for item in self.pois_objects:
             print(item.to_string())
